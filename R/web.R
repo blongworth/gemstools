d2b8c10ff4c589ffa0401d14a1a7d8a857433ff3
Brett Longworth <blongworth@whoi.edu>
2024-01-24 14:20:38 -0500

    new web read function

--- R/web.R
+++ R/web.R
@@ -1,12 +1,19 @@
 #' Read LECS data from website
 #'
-#' Reads HTML table from LECSrawdata into a single column data frame
+#' Reads HTML table from LECS_data into a single column data frame
 #'
+#' @param start_date Date to get date from. Date object or string coerceable
+#' to a date
 #' @return a single column data frame
 #' @export
 #'
-read_lecs_web <- function() {
-  xml2::read_html("https://gems.whoi.edu/LECSrawdata/") |>
+read_lecs_web <- function(start_date = NULL) {
+  if ( is.null(start_date) ) {
+    start_date = Sys.Date()
+  }
+  base_url <- "https://gems.whoi.edu/LECS_data/?timestamp="
+  query_url <- paste0(base_url, format(as.Date(start_date), "%Y%m%d%H"))
+  xml2::read_html(query_url) |>
     rvest::html_node("table") |>
     rvest::html_table()
 }
