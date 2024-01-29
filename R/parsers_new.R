5a900c2018a89958a4d10e9eb7f774b121089cf1
Brett Longworth <blongworth@whoi.edu>
2024-01-29 16:17:06 -0500

    fix types by field name

--- R/parsers_new.R
+++ R/parsers_new.R
@@ -30,8 +30,9 @@ lecs_post_times <- function(df) {
                       'day', 'month', 'year',
                       'lat', 'lon'),
              sep = ',') |>
-    mutate(across(5:10, as.integer),
-           across(11:12, as.numeric),
+    mutate(across(c('hour', 'min', 'sec',
+                      'day', 'month', 'year'), as.integer),
+           across(c('lat', 'lon'), as.numeric),
            timestamp = lubridate::make_datetime(year, month, day,
                                      hour, min, sec,
                                      tz = "America/New_York"),
@@ -54,8 +55,9 @@ lecs_met_data <- function(df) {
                       'PAR', 'wind_speed', 'wind_dir'),
              sep = ',') |>
     mutate(wind_dir = stringr::str_sub(wind_dir, 1, 6),
-           across(5:10, as.integer),
-           across(11:13, as.numeric),
+           across(c('hour', 'min', 'sec',
+                      'day', 'month', 'year'), as.integer),
+           across(c('PAR', 'wind_speed', 'wind_dir'), as.numeric),
            wind_speed = ifelse(wind_speed < 99, wind_speed, NA),
            wind_dir = ifelse(wind_dir < 360, wind_dir, NA),
            timestamp = lubridate::make_datetime(year, month, day,
@@ -76,29 +78,29 @@ lecs_status_data <- function(df) {
   df |>
     dplyr::filter(type == "S") |>
     tidyr::separate(data,
-             into = c('hour', 'min', 'sec', 'day', 'month', 'year',
-                      'adv_min', 'adv_sec', 'adv_day',
-                      'adv_hour', 'adv_year', 'adv_month',
-                      'bat', 'soundspeed', 'heading', 'pitch',
-                      'roll', 'temp',
-                      'pump_current', 'pump_voltage', 'pump_power'),
-             sep = ',') |>
+                    into = c('hour', 'min', 'sec', 'day', 'month', 'year',
+                             'adv_min', 'adv_sec', 'adv_day',
+                             'adv_hour', 'adv_year', 'adv_month',
+                             'bat', 'soundspeed', 'heading', 'pitch',
+                             'roll', 'temp',
+                             'pump_current', 'pump_voltage', 'pump_power'),
+                    sep = ',') |>
     mutate(pump_power = stringr::str_sub(pump_power, 1, 5),
            dplyr::across(c('hour', 'min', 'day', 'month', 'year',
-                  'adv_min', 'adv_day',
-                  'adv_hour', 'adv_year', 'adv_month'),
-           as.integer),
+                           'adv_min', 'adv_day',
+                           'adv_hour', 'adv_year', 'adv_month'),
+                         as.integer),
            dplyr::across(c('sec', 'adv_sec',
-                      'bat', 'soundspeed', 'heading', 'pitch',
-                      'roll', 'temp',
-                      'pump_current', 'pump_voltage', 'pump_power'), as.numeric),
+                           'bat', 'soundspeed', 'heading', 'pitch',
+                           'roll', 'temp',
+                           'pump_current', 'pump_voltage', 'pump_power'), as.numeric),
            dplyr::across(c('bat', 'soundspeed', 'heading', 'pitch',
-                      'roll', 'temp') , ~(.x) * .1),
+                           'roll', 'temp') , ~(.x) * .1),
            timestamp = lubridate::make_datetime(year, month, day, hour, min, sec,
-                                     tz = "America/New_York"),
+                                                tz = "America/New_York"),
            adv_timestamp = lubridate::make_datetime(adv_year + 2000, adv_month, adv_day,
-                                         adv_hour, adv_min, adv_sec,
-                                         tz = "America/New_York"))
+                                                    adv_hour, adv_min, adv_sec,
+                                                    tz = "America/New_York"))
 }
 
 #' parse LECS ADV data
@@ -114,23 +116,30 @@ lecs_adv_data <- function(df, rinko_cals) {
   df |>
     dplyr::filter(type == "D") |>
     tidyr::separate(data,
-             into = c('count', 'pressure',
-                      'x_vel', 'y_vel', 'z_vel',
-                      'x_amp', 'y_amp', 'z_amp',
-                      'x_cor', 'y_cor', 'z_cor',
-                      'ana_in', 'ana_in2', 'pH',
-                      'temp', 'oxy'),
-             sep = ',') |>
+                    into = c('count', 'pressure',
+                             'x_vel', 'y_vel', 'z_vel',
+                             'x_amp', 'y_amp', 'z_amp',
+                             'x_cor', 'y_cor', 'z_cor',
+                             'ana_in', 'ana_in2', 'pH',
+                             'temp', 'oxy'),
+                    sep = ',') |>
     mutate(oxy = stringr::str_sub(oxy, 1, 10),
-           dplyr::across(6:20, as.numeric),
-           count = as.integer(count),
+           dplyr::across(c('pressure',
+                           'temp', 'oxy'),
+                         as.numeric),
+           dplyr::across(c('count',
+                           'x_vel', 'y_vel', 'z_vel',
+                           'x_amp', 'y_amp', 'z_amp',
+                           'x_cor', 'y_cor', 'z_cor',
+                           'ana_in', 'ana_in2', 'pH'),
+                         as.integer),
            pressure = pressure / 65536 / 1000,
            temp = rinko_cals[["A"]] +
-                  temp * rinko_cals[["B"]] +
-                  temp ^ 2 * rinko_cals[["C"]] +
-                  temp ^ 3 * rinko_cals[["D"]],
+             temp * rinko_cals[["B"]] +
+             temp ^ 2 * rinko_cals[["C"]] +
+             temp ^ 3 * rinko_cals[["D"]],
            missing = dplyr::case_when(line == 1 | count > 255 | dplyr::lag(count) > 255 ~ NA_integer_,
-                               count > dplyr::lag(count) ~ count - 1L - dplyr::lag(count),
-                               TRUE ~ 255L + count - dplyr::lag(count)
-                             ))
+                                      count > dplyr::lag(count) ~ count - 1L - dplyr::lag(count),
+                                      TRUE ~ 255L + count - dplyr::lag(count)
+           ))
 }
