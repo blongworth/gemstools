94e8c24f161e7a5044e87de07be020bbf08200a8
Brett Longworth <blongworth@whoi.edu>
2024-03-14 11:44:41 -0400

    Add test to catch error adding time components

--- tests/testthat/test-time_alignment.R
+++ tests/testthat/test-time_alignment.R
@@ -11,6 +11,15 @@ test_that("make_lecs_ts() works for 21 lines of data", {
   expect_identical(adv_data_ts, adv_ts_21)
 })
 
+test_that("make_lecs_ts() adds time components", {
+  df <- readRDS(test_path("lecs_web.rds")) |>
+    head(21)
+  status <- lecs_status_data(df)
+  adv_data <- lecs_adv_data(df, rinko_cals)
+  adv_data_ts <- make_lecs_ts(adv_data, status)
+  expect_identical(adv_data_ts[['year']][1], 2024)
+})
+
 test_that("make_lecs_ts() works for 40 lines of data", {
   op <- options(digits.secs=3)
   on.exit(options(op), add = TRUE, after = FALSE)
