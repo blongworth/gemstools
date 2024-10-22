842d5d7a6434e4f4d6cb24c4c31218b6d969d875
Brett Longworth <blongworth@whoi.edu>
2024-10-22 11:31:23 -0400

    Remove duckdb tests

--- tests/testthat/test-processing.R
+++ tests/testthat/test-processing.R
@@ -4,25 +4,25 @@ test_that("Parsing lecs files works", {
   expect_true(is.list(ls))
 })
 
-test_that("Creating DuckDB works", {
-  lecs_data <- lecs_parse_file(test_path("test_file.txt"))
-  con <- lecs_create_db(lecs_data)
-  expect_s4_class(con, "duckdb_connection")
-  DBI::dbDisconnect(con, shutdown = TRUE)
-  file.remove(test_path(c("duckdb", "duckdb.wal")))
-})
-
-test_that("Populating DuckDB works", {
-  lecs_data <- lecs_parse_file(test_path("test_file.txt"))
-  con <- lecs_create_db(lecs_data)
-  lecs_parse_file_db(con, test_path("test_file.txt"))
-  count_adv <- DBI::dbGetQuery(con, "SELECT COUNT(*) as count_adv FROM adv") |>
-    dplyr::pull()
-  expect_equal(count_adv, 466)
-  lecs_parse_file_db(con, test_path("test_file.txt"))
-  count_adv <- DBI::dbGetQuery(con, "SELECT COUNT(*) as count_adv FROM adv") |>
-    dplyr::pull()
-  expect_equal(count_adv, 932)
-  DBI::dbDisconnect(con, shutdown = TRUE)
-  file.remove(test_path(c("duckdb", "duckdb.wal")))
-})
+# test_that("Creating DuckDB works", {
+#   lecs_data <- lecs_parse_file(test_path("test_file.txt"))
+#   con <- lecs_create_db(lecs_data)
+#   expect_s4_class(con, "duckdb_connection")
+#   DBI::dbDisconnect(con, shutdown = TRUE)
+#   file.remove(test_path(c("duckdb", "duckdb.wal")))
+# })
+#
+# test_that("Populating DuckDB works", {
+#   lecs_data <- lecs_parse_file(test_path("test_file.txt"))
+#   con <- lecs_create_db(lecs_data)
+#   lecs_parse_file_db(con, test_path("test_file.txt"))
+#   count_adv <- DBI::dbGetQuery(con, "SELECT COUNT(*) as count_adv FROM adv") |>
+#     dplyr::pull()
+#   expect_equal(count_adv, 466)
+#   lecs_parse_file_db(con, test_path("test_file.txt"))
+#   count_adv <- DBI::dbGetQuery(con, "SELECT COUNT(*) as count_adv FROM adv") |>
+#     dplyr::pull()
+#   expect_equal(count_adv, 932)
+#   DBI::dbDisconnect(con, shutdown = TRUE)
+#   file.remove(test_path(c("duckdb", "duckdb.wal")))
+# })
