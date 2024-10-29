8f5ebc67a9e575877b1efa8e0f7c9d0b22eb8df7
Brett Longworth <blongworth@whoi.edu>
2024-10-29 15:08:19 -0400

    make TZ explict. Adjust to UTC.

--- R/parsers.R
+++ R/parsers.R
@@ -55,7 +55,8 @@ lecs_post_times <- function(df) {
                       'day', 'month', 'year'), as.integer),
            across(c('lat', 'lon'), as.numeric),
            timestamp = lubridate::make_datetime(year, month, day,
-                                     hour, min, sec),
+                                     hour, min, sec, tz = "UTC"),
+           timestamp = timestamp + 3600 * 4, # convert to UTC
            row_count = row_num - lag(row_num)) |>
     select(timestamp, send, row_count)
 }
@@ -81,7 +82,8 @@ lecs_met_data <- function(df) {
            wind_speed = ifelse(wind_speed < 99, wind_speed, NA),
            wind_dir = ifelse(wind_dir < 360, wind_dir, NA),
            timestamp = lubridate::make_datetime(year, month, day,
-                                     hour, min, sec)) |>
+                                     hour, min, sec, tz = "UTC"),
+           timestamp = timestamp + 4 * 3600) |>
     select(-row_num, -type, -any_of("line"))
 }
 
@@ -127,9 +129,11 @@ lecs_status_data <- function(df) {
            dplyr::across(c('bat', 'soundspeed',
                            'heading', 'pitch', 'roll') , ~(.x) * .1),
            temp = temp * 0.01,
-           timestamp = lubridate::make_datetime(year, month, day, hour, min, sec),
+           timestamp = lubridate::make_datetime(year, month, day, hour, min, sec, tz = "UTC"),
+           timestamp = timestamp + 3600 * 4, # convert to UTC
            adv_timestamp = lubridate::make_datetime(adv_year + 2000, adv_month, adv_day,
-                                                    adv_hour, adv_min, adv_sec))
+                                                    adv_hour, adv_min, adv_sec, tz = "UTC"),
+           adv_timestamp = adv_timestamp + 3600 * 4) # convert to UTC
 }
 
 #' Clean status data
